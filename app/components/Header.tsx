5e08038ea9686765782bb5a04abe685255506311
hakeem-jc <hakeemclarke14@gmail.com>
2025-04-08 18:24:31 -0500

    feat: Update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,17 +9,13 @@ const Header = () => {
         </h5>
 
         <div className="items-center justify-between flex w-auto order-1">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-700 rounded-lg bg-gray-800 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
-            <li>
-              <a
-                href="https://www.hakeemclarke.com/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:p-0"
-                aria-current="page"
-              >
-                My Portfolio
-              </a>
-            </li>
-          </ul>
+          <a
+            href="https://www.hakeemclarke.com/"
+            className="block py-2 px-3 text-white bg-blue-600 rounded-sm"
+            aria-current="page"
+          >
+            More From Me
+          </a>
         </div>
       </div>
     </nav>
